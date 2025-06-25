6b346f0d920f7bf5b1250bf6ef226896823da20a
Nnamdionwukwe <nnamdionwukwe@gmail.com>
2025-06-25 14:06:21 -0700

    hu

--- src/component/Reviews.jsx
+++ src/component/Reviews.jsx
@@ -2,23 +2,19 @@ import { useState } from "react";
 import styles from "./Reviews.module.css";
 
 const steps = [
-  "bridal1.jpg",
-  "bridal3.jpg",
-  "bridal4.jpg",
-  "bridal5.jpg",
-  "lash2.jpg",
-  "lash1.jpg",
-  "lash3.jpg",
-
-  "wax4.jpg",
+  "I was extremely pleased with the service that the staff did for me. The office was attentive to my request and was great with communication. They were thorough and were able to tackle a messy situation. I would highly recommend their service.",
+  "The team came to do a deep cleaning at my house this week and they did an amazing job. We were beyond grateful since my husband and I work and study at the same time we have little to no time for cleaning the house and Molly Maid's team made our house look incredible. We are beyond happy and grateful with the team and their service.",
+  "Wow I am so impressed with Team #8 Araceli and Cristina! This is the first time I have had my home professionally cleaned and I can definitely say I will not hesitate to book with Molly Maid again. My home has never been cleaner and fresher. I would recommend Molly Maid to my friends and family in a heartbeat.",
+  "Our cleaning team arrived promptly and started cleaning immediately. They were very friendly and took great care to ensure they did a good job. Im very impressed with the company so far! From the initial in-home estimate to the first cleaning! Even having a supervisor stop by to ensure the cleaning was done properly. I look forward to continue to work with the company.",
+  "I have been using Molly Maid for a few months now and I am very happy with the service. The team is always on time, professional, and does a great job cleaning my home. I appreciate the attention to detail and the personalized service they provide. I highly recommend Molly Maid to anyone looking for a reliable and thorough cleaning service.",
 ];
 
 export default function Reviews() {
   const [step, setStep] = useState(1);
 
   function handleNext() {
-    if (step < 8) setStep(step + 1);
-    if (step >= 8) setStep(1);
+    if (step < 5) setStep(step + 1);
+    if (step >= 5) setStep(5);
   }
 
   //   setInterval(() => {
@@ -27,11 +23,11 @@ export default function Reviews() {
 
   function handlePrevious() {
     if (step > 1) setStep(step - 1);
-    if (step <= 1) setStep(8);
+    if (step <= 1) setStep(1);
   }
 
   return (
-    <div onClick={(e) => handlePrevious(e)} className={styles.main}>
+    <div className={styles.main}>
       <div className={styles.hearMain}>
         <div className={styles.hearMainSub}>
           <h1>Hear From Our Clients</h1>
@@ -39,12 +35,51 @@ export default function Reviews() {
         </div>
       </div>
 
-      <p>{steps[step - 1]}</p>
+      <div className={styles.reviewContainer}>
+        <div className={styles.imageContainer}>
+          <i class="fa fa-comment" aria-hidden="true"></i>
+        </div>
 
-      <i class="fa fa-star" aria-hidden="true"></i>
-      <i class="fa fa-comment" aria-hidden="true"></i>
-      <i className="fa fa-long-arrow-left" aria-hidden="true"></i>
-      <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
+        <p className={styles.reviews}>{steps[step - 1]}</p>
+
+        <div className={styles.reviewFooter}>
+          {step === 1 && <p className={styles.reviewName}>Michael Solace.</p>}
+          {step === 2 && <p className={styles.reviewName}>Johnson King.</p>}
+          {step == 3 && <p className={styles.reviewName}>Philip Evans.</p>}
+          {step === 4 && <p className={styles.reviewName}>Nnamdi Gideon.</p>}
+          {step === 5 && <p className={styles.reviewName}>Muhamed Ismail.</p>}
+
+          <div className={styles.reviewFooterSub}>
+            <h2>5/5</h2>
+
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.greaterContainer}>
+        <h2
+          onClick={handlePrevious}
+          className={step === 1 ? styles.h2 : styles.h1}
+        >
+          &lt;
+        </h2>
+
+        <div className={styles.circlesDiv}>
+          <p className={step === 1 ? styles.circles2 : styles.circles1}></p>
+          <p className={step === 2 ? styles.circles2 : styles.circles1}></p>
+          <p className={step === 3 ? styles.circles2 : styles.circles1}></p>
+          <p className={step === 4 ? styles.circles2 : styles.circles1}></p>
+          <p className={step === 5 ? styles.circles2 : styles.circles1}></p>
+        </div>
+        <h2 onClick={handleNext} className={step === 5 ? styles.h2 : styles.h1}>
+          &gt;
+        </h2>
+      </div>
     </div>
   );
 }
