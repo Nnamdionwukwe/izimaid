fc06f76c96f94d01ba7a521778124d27cc05dae9
Nnamdionwukwe <nnamdionwukwe@gmail.com>
2025-06-29 01:00:01 +0100

    gt

--- src/component/SecondPractically.jsx
+++ src/component/SecondPractically.jsx
@@ -86,6 +86,90 @@ export default function SecondPractically() {
           </div>
         </div>
       </div>
+
+      <div className={styles.main}>
+        <div className={styles.mainSub}>
+          <h1>Practically Spotless Blog</h1>
+
+          <div className={styles.mainh1}>
+            <h4>View All Blog Posts</h4>
+            <h5>&rarr;</h5>
+          </div>
+        </div>
+
+        <img
+          alt="clean"
+          src="https://img.freepik.com/premium-vector/housewife-cleaning-computer-table-with-duster-woman-wiping-workplace-desk-girl-dusting-housework-concept-modern-apartment-interior-female-cartoon-character_48369-25680.jpg"
+        />
+
+        <div className={styles.mainSub}>
+          <h3 className={styles.how}>
+            How Often Should I Have My House Cleaned?
+          </h3>
+
+          <p className={styles.there}>
+            There’s no hard-and-fast answer to the question of how often to have
+            your house cleaned. However, anyone thinking about hiring a cleaning
+            service must pin down a schedule that works for them. Here are some
+            factors to consider when deciding the frequency of professional
+            cleanings.
+          </p>
+
+          <div className={styles.mainh1}>
+            <h4>Learn More</h4>
+            <h5 className={styles.arrow}>&rarr;</h5>
+          </div>
+        </div>
+
+        <img
+          alt="clean"
+          src="https://media.istockphoto.com/id/1146263478/vector/flat-general-house-cleaning-vector-illustration.jpg?s=612x612&w=0&k=20&c=nNaC8t8yMNU13ldUEhzCiExcw5yrHYXiQwVkpbmfG0w="
+        />
+
+        <div className={styles.mainSub}>
+          <h3 className={styles.how}>
+            Cleaning Schedule For Working Moms and Dads
+          </h3>
+
+          <p className={styles.there}>
+            So how do you keep the house clean with a busy schedule? We have
+            compiled a list of the 10 best cleaning tips for moms and dads that
+            will help you manage cleaning and your busy schedule. This includes
+            ideas for creating a cleaning schedule and task list to keep the
+            house tidy without dedicating your entire night or weekend to
+            getting it done.
+          </p>
+
+          <div className={styles.mainh1}>
+            <h4>Learn More</h4>
+            <h5 className={styles.arrow}>&rarr;</h5>
+          </div>
+        </div>
+
+        <img
+          alt="clean"
+          src="https://cdn.vectorstock.com/i/500p/57/04/elderly-woman-cleaning-her-home-cartoon-vector-55805704.jpg"
+        />
+
+        <div className={styles.mainSub}>
+          <h3 className={styles.how}>
+            House Cleaning Tips for Seniors and Loved Ones
+          </h3>
+
+          <p className={styles.there}>
+            If you’ve been spending time cleaning for an elderly family member,
+            friend or loved one in your life, and are looking for ways to help
+            them stay self-sufficient, we have some advice to help you be more
+            efficient and, at the same time, reduce some of the unique risks
+            that seniors face.
+          </p>
+
+          <div className={styles.mainh1}>
+            <h4>Learn More</h4>
+            <h5 className={styles.arrow}>&rarr;</h5>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
