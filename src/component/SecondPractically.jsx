2d49da0a4455ae04d43826e9ad8de040c24b7ab0
Nnamdionwukwe <nnamdionwukwe@gmail.com>
2025-06-29 01:22:28 +0100

    lo

--- src/component/SecondPractically.jsx
+++ src/component/SecondPractically.jsx
@@ -89,30 +89,29 @@ export default function SecondPractically() {
 
       <div className={styles.main}>
         <div className={styles.mainSub}>
-          <h1>Practically Spotless Blog</h1>
+          <h1>Cleaning Tips</h1>
 
           <div className={styles.mainh1}>
-            <h4>View All Blog Posts</h4>
+            <h4>View All Cleaning Tips</h4>
             <h5>&rarr;</h5>
           </div>
         </div>
 
         <img
           alt="clean"
-          src="https://img.freepik.com/premium-vector/housewife-cleaning-computer-table-with-duster-woman-wiping-workplace-desk-girl-dusting-housework-concept-modern-apartment-interior-female-cartoon-character_48369-25680.jpg"
+          src="https://media.istockphoto.com/id/1330190659/vector/allergy-reaction-medicine-and-healthcare-concept.jpg?s=612x612&w=0&k=20&c=309Ayr3NzjTcXMN9zXaqHFD3rEUbJi1yxxXgS3EduC0="
         />
 
         <div className={styles.mainSub}>
           <h3 className={styles.how}>
-            How Often Should I Have My House Cleaned?
+            Cleaning Tips on How to Remove Allergens from Your Home
           </h3>
 
           <p className={styles.there}>
-            There’s no hard-and-fast answer to the question of how often to have
-            your house cleaned. However, anyone thinking about hiring a cleaning
-            service must pin down a schedule that works for them. Here are some
-            factors to consider when deciding the frequency of professional
-            cleanings.
+            While reducing allergens in your home is the goal, consistency is
+            the key to getting it done. Applying these cleaning tips
+            consistently will keep your home looking good, smelling fresh, and
+            reduce allergens in your home.
           </p>
 
           <div className={styles.mainh1}>
@@ -123,21 +122,19 @@ export default function SecondPractically() {
 
         <img
           alt="clean"
-          src="https://media.istockphoto.com/id/1146263478/vector/flat-general-house-cleaning-vector-illustration.jpg?s=612x612&w=0&k=20&c=nNaC8t8yMNU13ldUEhzCiExcw5yrHYXiQwVkpbmfG0w="
+          src="https://www.shutterstock.com/shutterstock/videos/1099981253/thumb/1.jpg?ip=x480"
         />
 
         <div className={styles.mainSub}>
           <h3 className={styles.how}>
-            Cleaning Schedule For Working Moms and Dads
+            Stain Removal Techniques: Tips & Guidelines
           </h3>
 
           <p className={styles.there}>
-            So how do you keep the house clean with a busy schedule? We have
-            compiled a list of the 10 best cleaning tips for moms and dads that
-            will help you manage cleaning and your busy schedule. This includes
-            ideas for creating a cleaning schedule and task list to keep the
-            house tidy without dedicating your entire night or weekend to
-            getting it done.
+            Stains can ruin furniture, carpet, tile, and many other items. But
+            that doesn’t have to be the case. Our stain removal guide will help
+            you remove even the most stubborn stains, regardless of the type of
+            stain and where it is found.
           </p>
 
           <div className={styles.mainh1}>
@@ -148,20 +145,17 @@ export default function SecondPractically() {
 
         <img
           alt="clean"
-          src="https://cdn.vectorstock.com/i/500p/57/04/elderly-woman-cleaning-her-home-cartoon-vector-55805704.jpg"
+          src="https://thealohahut.com/wp-content/uploads/2024/08/How-to-Help-Kids-with-Chores-Chore-Chart-2.webp"
         />
 
         <div className={styles.mainSub}>
-          <h3 className={styles.how}>
-            House Cleaning Tips for Seniors and Loved Ones
-          </h3>
+          <h3 className={styles.how}>Free Printable Kids Chore Charts</h3>
 
           <p className={styles.there}>
-            If you’ve been spending time cleaning for an elderly family member,
-            friend or loved one in your life, and are looking for ways to help
-            them stay self-sufficient, we have some advice to help you be more
-            efficient and, at the same time, reduce some of the unique risks
-            that seniors face.
+            Not sure which chores are appropriate for your young kids? Use Molly
+            Maid's Age-Appropriate Chores List to help you as they grow! For
+            toddlers, chores like putting away toys, and putting books and
+            magazines into a pile are a great start.
           </p>
 
           <div className={styles.mainh1}>
