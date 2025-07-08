527a6e86c9bb84953f677d1edbd4125e686cc407
Nnamdionwukwe <nnamdionwukwe@gmail.com>
2025-07-08 22:33:35 +0100

    gt

--- src/component/Faq.jsx
+++ src/component/Faq.jsx
@@ -6,12 +6,22 @@ export default function Faq() {
   const [isOpen2, setIsOpen2] = useState(false);
   const [isOpen3, setIsOpen3] = useState(false);
   const [isOpen4, setIsOpen4] = useState(false);
+  const [isOpen5, setIsOpen5] = useState(false);
+  const [isOpen6, setIsOpen6] = useState(false);
+  const [isOpen7, setIsOpen7] = useState(false);
+  const [isOpen8, setIsOpen8] = useState(false);
+  const [isOpen9, setIsOpen9] = useState(false);
 
   function handleClick1() {
     setIsOpen1((prev) => !prev);
     setIsOpen2(false);
     setIsOpen3(false);
     setIsOpen4(false);
+    setIsOpen5(false);
+    setIsOpen6(false);
+    setIsOpen7(false);
+    setIsOpen8(false);
+    setIsOpen9(false);
   }
 
   function handleClick2() {
@@ -33,6 +43,46 @@ export default function Faq() {
     setIsOpen1(false);
     setIsOpen2(false);
     setIsOpen3(false);
+    // setIsOpen1(false);
+  }
+
+  function handleClick5() {
+    setIsOpen5((prev) => !prev);
+    setIsOpen1(false);
+    setIsOpen2(false);
+    setIsOpen3(false);
+    setIsOpen4(false);
+    // setIsOpen1(false);
+  }
+  function handleClick6() {
+    setIsOpen6((prev) => !prev);
+    setIsOpen1(false);
+    setIsOpen2(false);
+    setIsOpen3(false);
+    setIsOpen4(false);
+    // setIsOpen1(false);
+  }
+  function handleClick7() {
+    setIsOpen4((prev) => !prev);
+    setIsOpen1(false);
+    setIsOpen2(false);
+    setIsOpen3(false);
+    // setIsOpen1(false);
+  }
+  function handleClick8() {
+    setIsOpen8((prev) => !prev);
+    setIsOpen1(false);
+    setIsOpen2(false);
+    setIsOpen3(false);
+    setIsOpen4(false);
+    // setIsOpen1(false);
+  }
+  function handleClick9() {
+    setIsOpen9((prev) => !prev);
+    setIsOpen1(false);
+    setIsOpen2(false);
+    setIsOpen3(false);
+    setIsOpen4(false);
     // setIsOpen1(false);
   }
 
@@ -156,6 +206,159 @@ export default function Faq() {
           </div>
         )}
       </div>
+
+      <div className={styles.mainSub}>
+        <div onClick={handleClick5} className={styles.backed}>
+          <h3>What Is Our Home Cleaning Process?</h3>
+
+          {isOpen5 ? <h2>&minus;</h2> : <h1>&#43;</h1>}
+        </div>
+
+        {isOpen5 && (
+          <div className={styles.residentialDIv}>
+            <p className={styles.hoverMainliv1}>
+              Our cleaning process is easy and thorough. First, our friendly
+              team arrives on time, ready to make your home shine. We start by
+              cleaning up each room and dusting all surfaces while removing dirt
+              and allergens. We clean countertops, appliances, and the sink in
+              the kitchen so it shines. Bathrooms can get messy quickly, so we
+              give them special attention, from scrubbing toilets to cleaning
+              counters and wiping mirrors.
+            </p>
+
+            <p className={styles.hoverMainliv1}>
+              If you’re looking for
+              <span className={styles.span}> cleaning services near me</span>
+              that cover it all, Molly Maid is the answer. We ensure that floors
+              are vacuumed and mopped throughout the house, making them clean
+              and fresh. Want your bedrooms to be cozy retreats? Come home to
+              neatly made beds and refreshed rooms. We also tackle additional
+              spaces, like hallways and entryways, to ensure no areas are
+              overlooked. page to determine the services that best suit your
+              needs.
+            </p>
+
+            <p className={styles.hoverMainliv1}>
+              Whether you’re looking for recurring cleaning services, occasional
+              cleaning, or helping with cleaning during a move, our goal is to
+              leave your home spotless and inviting. We take care of the details
+              so you can relax and enjoy your clean, comfortable space.
+            </p>
+          </div>
+        )}
+      </div>
+
+      <div className={styles.mainSub}>
+        <div onClick={handleClick6} className={styles.backed}>
+          <h3>What Other Services Does Molly Maid Provide?</h3>
+
+          {isOpen6 ? <h2>&minus;</h2> : <h1>&#43;</h1>}
+        </div>
+
+        {isOpen6 && (
+          <div className={styles.residentialDIv}>
+            <p className={styles.hoverMainliv1}>
+              We offer cleaning services for any occasion. For a list of all the
+              cleaning services our teams provide, visit
+              <span className={styles.span}> our services</span>
+              page to determine the services that best suit your needs.
+            </p>
+          </div>
+        )}
+      </div>
+
+      <div className={styles.mainSub}>
+        <div onClick={handleClick7} className={styles.backed}>
+          <h3>Why Do We Stand Behind Our Work?</h3>
+
+          {isOpen7 ? <h2>&minus;</h2> : <h1>&#43;</h1>}
+        </div>
+
+        {isOpen7 && (
+          <div className={styles.residentialDIv}>
+            <p className={styles.hoverMainliv1}>
+              Molly Maid doesn’t just promise a thorough clean, we back it up.
+              <span className={styles.span}>
+                The Neighborly Done Right Promise®
+              </span>
+              is our commitment to excellence, ensuring that our cleaning
+              services for residential homes are held to the highest standards.
+              We prioritize your satisfaction and the quality of our services.
+              From the moment we step into your home, we bring professionalism
+              and cleaning expertise.
+            </p>
+
+            <p className={styles.hoverMainliv1}>
+              Our skilled team treats your space with care, ensuring thorough
+              cleaning throughout your home. If something doesn't meet your
+              expectations, we'll make it right.
+            </p>
+
+            <p className={styles.hoverMainliv1}>
+              We value your trust and aim to provide exceptional service that
+              leaves your home gleaming and you smiling.
+            </p>
+
+            <p className={styles.hoverMainliv1}>
+              Your cleanliness satisfaction is always our top priority.
+            </p>
+          </div>
+        )}
+      </div>
+
+      <div className={styles.mainSub}>
+        <div onClick={handleClick8} className={styles.backed}>
+          <h3>
+            How Do We Provide a Thorough Clean When and Where You Need It?
+          </h3>
+
+          {isOpen8 ? <h2>&minus;</h2> : <h1>&#43;</h1>}
+        </div>
+
+        {isOpen8 && (
+          <div className={styles.residentialDIv}>
+            <p className={styles.hoverMainliv1}>
+              Customize your cleaning services as a part of your one-time or
+              recurring cleaning when things get grimy! In addition to our
+              standard room-by-room cleaning services, our team can also invest
+              extra time cleaning your home's most essential appliances. Check
+              out our refrigerator cleaning and oven cleaning options to really
+              make your kitchen shine. Rooms included with our home cleaning
+              service:
+            </p>
+
+            <ul className={styles.hoverMainliv1}>
+              <li>Bathroom cleaning</li>
+              <li>Bedroom cleaning</li>
+              <li>Kitchen cleaning</li>
+              <li>Laundry room cleaning</li>
+              <li>Dining room cleaning</li>
+            </ul>
+          </div>
+        )}
+      </div>
+
+      <div className={styles.mainSub}>
+        <div onClick={handleClick9} className={styles.backed}>
+          <h3>Do I Need To Be Home During My Appointment?</h3>
+
+          {isOpen9 ? <h2>&minus;</h2> : <h1>&#43;</h1>}
+        </div>
+
+        {isOpen9 && (
+          <div className={styles.residentialDIv}>
+            <p className={styles.hoverMainliv1}>
+              Most of our residential cleaning services occur during the day
+              when our clients are away or at work. Our house cleaning
+              professionals are usually entrusted with a key or garage code to
+              make things easy for our clients. Whichever option you prefer,
+              we’ll work with you to meet your specific cleaning needs.
+            </p>
+          </div>
+        )}
+      </div>
+
+      <h3>See All FAQs</h3>
     </div>
   );
 }
