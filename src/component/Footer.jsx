80abf6e263bff23a04aca5fbf219abdbb56b24fc
Nnamdionwukwe <nnamdionwukwe@gmail.com>
2025-07-14 01:07:21 +0100

    fr

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,8 +3,11 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <div>
-      {" "}
-      <img className={styles.logo} alt="Logo" src="izimaid.jpg" />
+      <div className={styles.footerDiv}>
+        <img className={styles.logo} alt="Logo" src="izimaid.jpg" />
+
+        <h2>0803 058 8779</h2>
+      </div>
     </div>
   );
 }
