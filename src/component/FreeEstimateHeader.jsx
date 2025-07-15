f6f49bff7488ed000dd4f3cea94d00df9f41e5e1
Nnamdionwukwe <nnamdionwukwe@gmail.com>
2025-07-15 12:27:00 +0100

    gt

---
+++ src/component/FreeEstimateHeader.jsx
@@ -0,0 +1,38 @@
+import styles from "./RequestEstimate.module.css";
+
+export default function FreeEstimateHeader() {
+  return (
+    <>
+      <div className={styles.SubHeader1}>
+        <a href="https://izimaid-sage.vercel.app">
+          <img className={styles.logo} alt="Logo" src="izimaid.jpg" />
+        </a>
+
+        <div className={styles.hamburger}>
+          <div className={styles.sub2}>
+            <i class="fa fa-phone" aria-hidden="true"></i>
+            <p>Call Us</p>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className={styles.SubMain}>
+        <div className={styles.SubHeader2}>
+          <div className={styles.sub1}>
+            <i class="fa fa-calendar" aria-hidden="true"></i>
+
+            <p className={styles.subP}>Request a Free Estimate</p>
+          </div>
+
+          <div className={styles.sub2}>
+            <i class="fa fa-phone" aria-hidden="true"></i>
+            <p>Call Us</p>
+          </div>
+          <div className={styles.minsDiv}>
+            <p className={styles.mins}>In under 2 mins</p>
+          </div>
+        </div>
+      </div> */}
+    </>
+  );
+}
--- src/component/RequestEstimate.jsx
+++ src/component/RequestEstimate.jsx
@@ -1,3 +1,16 @@
+import FirstHeader from "./FirstHeader";
+import FixedHeader from "./FixedHeader";
+import FreeEstimateHeader from "./FreeEstimateHeader";
+import styles from "./RequestEstimate.module.css";
+
 export default function RequestEstimate() {
-    return <div>Request a Free Estimate</div>
+  return (
+    <div>
+      <FirstHeader />
+
+      <div className={styles.fixedHeade}>
+        <FreeEstimateHeader />
+      </div>
+    </div>
+  );
 }
